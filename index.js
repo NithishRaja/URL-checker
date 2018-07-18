b47884da786d9bc333a3c3bb374268699dbcb851
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-07-18 14:55:43 +0530

    create server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("let's see");
+// primary file for server
+
+// dependencies
+const http = require("http");
+
+// creating server
+const server = http.createServer(function(req, res){
+  res.end("responding to request!");
+});
+
+// listen to port 8000
+server.listen(8000, function(){
+  console.log("server is listening to port 8000, press ctrl+c to exit");
+});
