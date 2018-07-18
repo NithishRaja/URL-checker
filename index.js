0e7cc87d57ad94a1e8a7dd19bb8152c9a04a7b66
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-07-18 21:28:45 +0530

    listen and get data

--- index.js
+++ index.js
@@ -3,18 +3,35 @@
 // dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // creating server
 const server = http.createServer(function(req, res){
-  // printing req object to console
+  // parsing url
   var parsedUrl = url.parse(req.url, true);
+  // getting trimmed url
   var trimmedUrl = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
+  // getting request method
   var method = req.method;
+  // getting query object
   var query = parsedUrl.query;
-  console.log(trimmedUrl+" "+method);
-  console.log(query);
-  // sending response
-  res.end("responding to request!");
+  // getting headers
+  var headers = req.headers;
+  // getting req body
+  var decoder = new StringDecoder("utf-8");
+  var buffer="";
+  // listening to data stream
+  req.on("data", function(data){
+    buffer+=decoder.write(data);
+  });
+  // listening to end of data stream
+  req.on("end", function(){
+    buffer+=decoder.end();
+    // logging req body
+    console.log(buffer);
+    // sending response
+    res.end("responding to request!");
+  });
 });
 
 // listen to port 8000
