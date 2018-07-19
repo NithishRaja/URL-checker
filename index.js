0d5ff0f0ff80f1e05314cb33f55a6d484c1d2599
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-07-19 14:32:21 +0530

    add routing logic

--- index.js
+++ index.js
@@ -27,10 +27,32 @@ const server = http.createServer(function(req, res){
   // listening to end of data stream
   req.on("end", function(){
     buffer+=decoder.end();
-    // logging req body
-    console.log(buffer);
-    // sending response
-    res.end("responding to request!");
+
+    // getting the route handler
+    var selectedHandler = typeof(router[trimmedUrl])!="undefined"?router[trimmedUrl]:handler.notFound;
+    // constructing data object to send to handler
+    const data = {
+      trimmedUrl: trimmedUrl,
+      method: method,
+      query: query,
+      headers: headers,
+      payload: buffer
+    };
+    // routing request to selectedHandler
+    selectedHandler(data, function(statusCode,payload){
+      // setting default value of 200 for statusCode
+      statusCode = typeof(statusCode) == "number"?statusCode:200;
+      // setting empty object as default for payload
+      payload = typeof(payload) == "object"?payload:{};
+      // converting payload from object to string
+      const payloadString = JSON.stringify(payload);
+
+      // sending response with status code and payload
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      // logging the response
+      console.log("response: ", statusCode, payloadString);
+    });
   });
 });
 
@@ -38,3 +60,21 @@ const server = http.createServer(function(req, res){
 server.listen(8000, function(){
   console.log("server is listening to port 8000, press ctrl+c to exit");
 });
+
+// defining handler
+var handler = {};
+
+// sample handler for the route /sample
+handler.sample = function(data, callback){
+  callback(200, {'name': 'sample handler'});
+};
+
+// not found handler for default
+handler.notFound = function(data, callback){
+  callback(404);
+}
+
+// defining router
+const router = {
+  'sample': handler.sample,
+}
