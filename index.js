d70a82ca3d12ba7e9a889969921556b19169b9ca
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-07-22 16:01:43 +0530

    add logic to update file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const _data = require("./lib/data");
 
 // Code for testing data library
 // TODO: delete at end of testing
-_data.create("indexx", "folder",{message: "qzawsxedrcftvgbhnjmk"}, function(err, payload){
+_data.update("indexx", "folder",{message: "i am here!!!"}, function(err, payload){
   if(!err && payload){
     var payloadString = JSON.stringify(payload);
     console.log(payload);
