96f7880e4232ae8a8e3bb100ee153ef891d4b10c
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-02 14:38:52 +0530

    add logic to get method of users route

--- router/handler/users/get.js
+++ router/handler/users/get.js
@@ -3,9 +3,27 @@
  *
  */
 
+// Dependencies
+const _data = require("./../../../lib/data");
+
 // Initializing get function
 const get = function(data, callback){
-  callback(200, {message: "get"});
+  // Checking if file exists
+  const phone = typeof(data.query.phone)=="string"&&data.query.phone.trim().length==10?data.query.phone.trim():false;
+  if(phone){
+    // Reading file
+    _data.read(phone,"users",function(err, data){
+      if(!err){
+        // removing hashedPassword
+        delete data.hashedPassword;
+        callback(200, data);
+      }else{
+        callback(500, {"Error":"Unable to read file"});
+      }
+    });
+  }else{
+    callback(404);
+  }
 }
 
 // Exporting module
