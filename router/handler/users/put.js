a081b8a79570ce24f6ac1bb63ec1f2b8b4aed0e3
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-02 16:12:02 +0530

    add logic for put method of users route

--- router/handler/users/put.js
+++ router/handler/users/put.js
@@ -3,9 +3,50 @@
  *
  */
 
+// Dependencies
+const _data = require("./../../../lib/data");
+const _helpers = require("./../../../lib/helpers");
+
 // Initializing put function
 const put = function(data, callback){
-  callback(200, {message: "put"});
+  // Checking phone field validity
+  const phone = typeof(data.payload.phone)=="string"&&data.payload.phone.trim().length==10?data.payload.phone.trim():false;
+  if(phone){
+    // Checking optional details
+    const firstName = typeof(data.payload.firstName)=="string"&&data.payload.firstName.trim().length>0?data.payload.firstName.trim():false;
+    const lastName = typeof(data.payload.lastName)=="string"&&data.payload.lastName.trim().length>0?data.payload.lastName.trim():false;
+    const password = typeof(data.payload.password)=="string"&&data.payload.password.trim().length>0?data.payload.password.trim():false;
+    // Reading from file
+    _data.read(phone, "users", function(err,userData){
+      if(!err){
+        // Updating user data
+        if(firstName){
+          userData.firstName = firstName;
+        }
+        if(lastName){
+          userData.lastName = lastName;
+        }
+        if(password){
+          userData.hashedPassword = _helpers.hash(password);
+        }
+        // Writing updates to file
+        _data.update(phone, "users", userData, function(err){
+          console.log(userData);
+          if(!err){
+            callback(200);
+          }else{
+            console.log(err);
+            callback(500, {"Error":"Unable to update file"});
+          }
+        });
+      }else{
+        console.log(err);
+        callback(500, {"Error":"Unable to access file"});
+      }
+    });
+  }else{
+    callback(400, {"Error": "Missing fields"});
+  }
 }
 
 // Exporting module
