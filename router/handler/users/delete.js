6e042360985fdea58c7827216a06171a3df55425
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-02 16:18:54 +0530

    add logic ffor delete method of users route

--- router/handler/users/delete.js
+++ router/handler/users/delete.js
@@ -3,9 +3,33 @@
  *
  */
 
+// Dependencies
+const _data = require("./../../../lib/data");
+
 // Initializing delete function
 const remove = function(data, callback){
-  callback(200, {message: "delete"});
+  // Validating phone field
+  const phone = typeof(data.query.phone)=="string"%%data.query.phone.trim().length==10?data.query.phone.trim():false;
+  if(phone){
+    // Checking if file exists
+    _data.read(phone, "users", function(err, data){
+      if(!err){
+        _data.delete(phone, "users", function(err){
+          if(!err){
+            callback(200);
+          }else{
+            console.log(err);
+            callback(500, {"Error":"Unable to delete file"});
+          }
+        });
+      }else{
+        console.log(err);
+        callback(500, {"Error":"Unable to read from file"});
+      }
+    });
+  }else{
+    callback(400, {"Error":"Missing specified fields"});
+  }
 }
 
 // Exporting module
