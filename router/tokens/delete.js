8ff660613dbbd10f6cf654299ad2e72f86cfaed8
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-10 19:34:33 +0530

    add logic to delete method of tokens route

--- router/tokens/delete.js
+++ router/tokens/delete.js
@@ -3,9 +3,31 @@
  *
  */
 
+// Dependencies
+const _data = require("./../../lib/data");
+
 // Initializing function
 const remove = function(data, callback){
-  callback(200, {message: "delete"});
+  const tokenId = typeof(data.query.tokenId)=="string"&&data.query.tokenId.trim().length==20?data.query.tokenId.trim():false;
+  if(tokenId){
+    _data.read(tokenId, "tokens", function(err, tokenData){
+      if(!err){
+        _data.delete(tokenId, "tokens", function(err){
+          if(!err){
+            callback(200);
+          }else{
+            console.log(err);
+            callback(500, {"Error":"Unable to delete token"});
+          }
+        });
+      }else{
+        console.log(err);
+        callback(400, {"Error":"Token does not exist"});
+      }
+    });
+  }else{
+    callback(400, {"Error":"Required fields missing"});
+  }
 };
 
 // Exporting function
