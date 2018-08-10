b6918c6d1cbc899dbe174622567b91b85d21cb24
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-10 19:50:15 +0530

    add validation to get method of users route

--- router/users/get.js
+++ router/users/get.js
@@ -5,20 +5,30 @@
 
 // Dependencies
 const _data = require("./../../lib/data");
+const _helpers = require("./../../lib/helpers");
 
 // Initializing get function
 const get = function(data, callback){
   // Checking if file exists
   const phone = typeof(data.query.phone)=="string"&&data.query.phone.trim().length==10?data.query.phone.trim():false;
   if(phone){
-    // Reading file
-    _data.read(phone,"users",function(err, data){
-      if(!err){
-        // removing hashedPassword
-        delete data.hashedPassword;
-        callback(200, data);
+    // Getting user token
+    const token = typeof(data.headers.token)=="string"&&data.headers.token.trim().length==20?data.headers.token.trim():false;
+    // Checking validity of token
+    _helpers.verifyToken(token, phone, function(validity){
+      if(validity){
+        // Reading file
+        _data.read(phone,"users",function(err, data){
+          if(!err){
+            // removing hashedPassword
+            delete data.hashedPassword;
+            callback(200, data);
+          }else{
+            callback(500, {"Error":"Unable to read file"});
+          }
+        });  
       }else{
-        callback(500, {"Error":"Unable to read file"});
+        callback(403, {"Error":"Token has expired or token does not exist"});
       }
     });
   }else{
