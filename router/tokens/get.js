ed3ccb34feff4644df927e2a18969c8aaffb5f05
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-10 13:20:22 +0530

    add logic to get method of tokens route

--- router/tokens/get.js
+++ router/tokens/get.js
@@ -3,9 +3,26 @@
  *
  */
 
+// Dependencies
+const _data = require("./../../lib/data");
+
 // Initializing function
 const get = function(data, callback){
-  callback(200, {message: "get"});
+  const tokenId = typeof(data.query.tokenId)=="string"&&data.query.tokenId.trim().length==20?data.query.tokenId.trim():false;
+  // Checking validity of token id
+  if(tokenId){
+    // Reading token
+    _data.read(tokenId, "tokens", function(err, tokenObject){
+      if(!err){
+        callback(200, tokenObject);
+      }else{
+        console.log(err);
+        callback({"Error":"Token does not exists"});
+      }
+    });
+  }else{
+    callback(400, {"Error":"Missing required fields"});
+  }
 };
 
 // Exporting function
