b6104b1af2258308ab423fa5fc9dcaf1139973f7
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-10 12:29:30 +0530

    add basic logic for post method of tokens route

--- router/tokens/post.js
+++ router/tokens/post.js
@@ -3,9 +3,33 @@
  *
  */
 
+// Dependencies
+const _data = require("./../../lib/data");
+const _helpers = require("./../../lib/helpers");
+
 // Initializing function
 const post = function(data, callback){
-  callback(200, {message: "post"});
+  const phone = typeof(data.payload.phone) =="string"&&data.payload.phone.trim().length==10?data.payload.phone.trim():false;
+  const password = typeof(data.payload.password)=="string"&&data.payload.password.trim().length>0?data.payload.password.trim():false;
+  // Checking if payload containes required data
+  if(phone && password){
+    _data.read(phone, "tokens", function(err, dataObject){
+      if(!err){
+        // Hashing the password sent by user
+        const hashedPassword = _helpers.hash(password);
+        // Checking if hashed password matches with existing password
+        if(hashedPassword==dataObject.hashedPassword){
+          
+        }else{
+          callback(400, {"Error":"Password does not match"});
+        }
+      }else{
+        callback(400, {"Error":"User does not exist"});
+      }
+    });
+  }else{
+    callback(400, {"Error":"required fields not filled"});
+  }
 };
 
 // Exporting function
