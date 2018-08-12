9b774dab93d4c7d9b8cd7299683c941427d5c767
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-12 10:27:52 +0530

    add sanity checking logic to post method of checks route

--- router/checks/post.js
+++ router/checks/post.js
@@ -4,10 +4,24 @@
  */
 
 // Dependencies
+const _data = require("./../../lib/data");
 
 // Initializing function
 const post = function(data, callback){
-  callback(200, {"message":"post"});
+  // Validating payload
+  const protocol = typeof(data.payload.protocol)=="string"&&["http","https"].indexOf(data.payload.protocol.trim())>-1?data.payload.protocol.trim():false;
+  const method = typeof(data.payload.method)=="string"&&["get","put","post","delete"].indexOf(data.payload.method.trim())>-1?data.payload.method.trim():false;
+  const url = typeof(data.payload.url)=="string"&&data.payload.url.trim().length>0?data.payload.url.trim():false;
+  const successCodes = typeof(data.payload.successCodes)=="object"&&data.payload.successCodes instanceof Array?data.payload.successCodes:false;
+  const timeout = typeof(data.payload.timeout)=="number"&&data.payload.timeout>=1&&data.payload.timeout<=5?data.payload.timeout:false;
+  // Continuee only if payload is valid
+  if(protocol&&method&&url&&successCodes&&timeout){
+    // Getting token
+    const token = typeof(data.headers.token)=="string"&&data.headers.token.trim().length==20?data.headers.token.trim():false;
+    callback(200);
+  }else{
+    callback(400, {"Error":"Missing required fields"});
+  }
 };
 
 // Exporting function
