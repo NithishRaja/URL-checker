3e7a6a01ea388d4e3875931071bd49d1853fc376
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-12 10:08:49 +0530

    add logic to identify calling method

--- router/checks/index.js
+++ router/checks/index.js
@@ -10,12 +10,20 @@ const post = require("./post");
 const remove = require("./delete");
 
 // Initializing container
-const container = {
+const method = {
   get: get,
   put: put,
   post: post,
   delete: remove
 };
 
+const checks = function(data, callback){
+  if(Object.keys(method).indexOf(data.method.toLowerCase())>-1){
+    method[data.method.toLowerCase()](data, callback);
+  }else{
+    callback(405, {"Error": "Method not allowed"});
+  }
+};
+
 // Exporting container
-module.exports = container;
+module.exports = checks;
