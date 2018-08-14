5a85c9cf4de536c45c9d42aa15bff07cf779d9a5
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-14 18:37:54 +0530

    refactor server file

--- index.js
+++ index.js
@@ -1,74 +1,22 @@
 /*
- * primary file for server
+ * Primary file for API
  *
  */
 
-// dependencies
-const http = require("http");
-const url = require("url");
-const StringDecoder = require("string_decoder").StringDecoder;
-const config = require("./config");
-const router = require("./router");
-const _helpers = require("./lib/helpers");
+// Dependencies
+const server = require("./lib/server");
 
-// defining server logic
-const ServerLogic = function(req, res){
-  // parsing url
-  var parsedUrl = url.parse(req.url, true);
-  // getting trimmed url
-  var trimmedUrl = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
-  // getting request method
-  var method = req.method;
-  // getting query object
-  var query = parsedUrl.query;
-  // getting headers
-  var headers = req.headers;
-  // getting req body
-  var decoder = new StringDecoder("utf-8");
-  var buffer="";
-  // listening to data stream
-  req.on("data", function(data){
-    buffer+=decoder.write(data);
-  });
-  // listening to end of data stream
-  req.on("end", function(){
-    buffer+=decoder.end();
+// Declaring app
+var app = {};
 
-    // getting the route handler
-    var selectedHandler = typeof(router[trimmedUrl])!="undefined"?router[trimmedUrl]:router.notFound;
-    // constructing data object to send to handler
-    const data = {
-      trimmedUrl: trimmedUrl,
-      method: method,
-      query: query,
-      headers: headers,
-      payload: _helpers.parse(buffer)
-    };
-    // routing request to selectedHandler
-    selectedHandler(data, function(statusCode,payload){
-      // setting default value of 200 for statusCode
-      statusCode = typeof(statusCode) == "number"?statusCode:200;
-      // setting empty object as default for payload
-      payload = typeof(payload) == "object"?payload:{};
-      // converting payload from object to string
-      const payloadString = JSON.stringify(payload);
+// Initializing init function
+app.init = function(){
+  // Starting server
+  server.init();
+};
 
-      // sending response with status code and payload
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(statusCode);
-      res.end(payloadString);
-      // logging the response
-      console.log("response: ", statusCode, payloadString);
-    });
-  });
-}
+// Starting the app
+app.init();
 
-// creating server
-const httpServer = http.createServer(function(req, res){
-  ServerLogic(req, res);
-});
-
-// listen to port 8000
-httpServer.listen(config.port, function(){
-  console.log("HTTP server is listening to port "+config.port+" in "+config.envName+" mode, press ctrl+c to exit");
-});
+// Exporting the app
+module.exports = app;
