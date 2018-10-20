9dff0b6d69ca7b3f52df1c3c1301c2d1b5bd43bc
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-10-20 16:04:31 +0530

    update config with global values

--- config.js
+++ config.js
@@ -22,6 +22,11 @@ environments.development = {
     'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
     'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
     'fromPhone' : '+15005550006'
+  },
+  global: {
+    appName: "URL checker",
+    yearCreated: "2018",
+    companyName: "Mamba.co"
   }
 };
 
